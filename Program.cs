0010d0b7b7c15f78ac36a6eb00425ef4618a5a51
Jzurcc <johnreniel042206@gmail.com>
2024-02-07 16:07:44 +0800

    Initialized repository

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Media;
+using static System.Threading.Thread;
+
+class Program {
+static void Main() {
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine(@"
+ ▄████████    ▄█    █▄     ▄██████▄   ▄██████▄     ▄████████    ▄████████         ▄████████      ████████▄     ▄████████  ▄█      ███     ▄██   ▄   
+███    ███   ███    ███   ███    ███ ███    ███   ███    ███   ███    ███        ███    ███      ███   ▀███   ███    ███ ███  ▀█████████▄ ███   ██▄ 
+███    █▀    ███    ███   ███    ███ ███    ███   ███    █▀    ███    █▀         ███    ███      ███    ███   ███    █▀  ███▌    ▀███▀▀██ ███▄▄▄███ 
+███         ▄███▄▄▄▄███▄▄ ███    ███ ███    ███   ███         ▄███▄▄▄            ███    ███      ███    ███  ▄███▄▄▄     ███▌     ███   ▀ ▀▀▀▀▀▀███ 
+███        ▀▀███▀▀▀▀███▀  ███    ███ ███    ███ ▀███████████ ▀▀███▀▀▀          ▀███████████      ███    ███ ▀▀███▀▀▀     ███▌     ███     ▄██   ███ 
+███    █▄    ███    ███   ███    ███ ███    ███          ███   ███    █▄         ███    ███      ███    ███   ███    █▄  ███      ███     ███   ███ 
+███    ███   ███    ███   ███    ███ ███    ███    ▄█    ███   ███    ███        ███    ███      ███   ▄███   ███    ███ ███      ███     ███   ███ 
+████████▀    ███    █▀     ▀██████▀   ▀██████▀   ▄████████▀    ██████████        ███    █▀       ████████▀    ██████████ █▀      ▄████▀    ▀█████▀ 
+
+");
+    Print("Hello there follow citizen! I am your guide!", 20, 1500);
+    Print("Oh no! What have you done?!");
+    Print("Ohohoho! What an interesting sight!");
+    Main();
+}
+
+public static void Print(string str, int speed = 40, int duration = 1000, ConsoleColor color = ConsoleColor.White) {
+    Console.ForegroundColor = color;
+    foreach (char c in str) {
+        Console.Write(c);
+        Sleep(speed); 
+    }
+    Console.WriteLine();
+    Sleep(duration);
+}
+
+}
