849168f4055f35adc1bc424abeb92cb37e6b51a0
Jzurcc <johnreniel042206@gmail.com>
2024-02-14 17:27:50 +0800

    Damage balance fixes

--- obj/Debug/net8.0/Choose A Deity.AssemblyInfo.cs
+++ obj/Debug/net8.0/Choose A Deity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Choose A Deity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d54070bc79059adb41150da64fbcc5c84e9de11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2912760d33d4b2acc3bc9ece105219d7bb3f3a36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Choose A Deity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Choose A Deity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
